656f770da71920d91a90403e61f52272df86dfc6
Andrey <andreytelushko@gmail.com>
2023-01-18 21:07:58 +0300

    SquareNumber

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int SquareNumber(int a, int b)
+            {
+                int result = 1;
+                for (int i = 1; i <= b; i++)
+                {
+                    result = result * a;
+                }
+                return result;
+            }
+            Console.WriteLine("Возведение числа в натуральную степень.");
+            Console.Write("Введите число: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+
+            int squarenumber = SquareNumber(numberA, numberB);
+            Console.WriteLine("Ответ: " + squarenumber);
+        }
+    }
+
+}
